b993590938934e38f4ab152439456c1ee532915c
bmbmbm9 <bmbmbm9@protonmail.com>
2023-05-19 01:43:53 +0300

    init project with migration and user table

---
+++ src/test/java/ua/knu/fit/sydorenko/secureapi/SecureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.knu.fit.sydorenko.secureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
