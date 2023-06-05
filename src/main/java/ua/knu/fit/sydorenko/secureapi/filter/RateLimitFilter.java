e722b92e4dd33269ed7586030e391d6c7c41f485
bmbmbm9 <bmbmbm9@protonmail.com>
2023-06-05 23:34:32 +0300

    deleted unused from filter

--- src/main/java/ua/knu/fit/sydorenko/secureapi/filter/RateLimitFilter.java
+++ src/main/java/ua/knu/fit/sydorenko/secureapi/filter/RateLimitFilter.java
@@ -3,21 +3,19 @@ package ua.knu.fit.sydorenko.secureapi.filter;
 import io.github.bucket4j.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
-import ua.knu.fit.sydorenko.secureapi.exception.AuthException;
 import ua.knu.fit.sydorenko.secureapi.exception.RateLimitException;
 
 import java.time.Duration;
 
 @Configuration
 @Slf4j
-public class RateLimitFilter implements WebFilter, Ordered {
+public class RateLimitFilter implements WebFilter {
 
     private Bucket bucket;
 
@@ -40,11 +38,6 @@ public class RateLimitFilter implements WebFilter, Ordered {
         }
         response.getHeaders().set("X-Rate-Limit-Remaining", String.valueOf(remainingLimit));
         return chain.filter(exchange);
-    }
-
-    @Override
-    public int getOrder() {
-        return -1;
     }
 
     public Bucket getBucket() {
